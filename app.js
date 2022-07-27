93fa54085e0549a3ef9681a59312d905b054e254
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-27 10:26:50 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 // Web Server
-
 const http = require("http");
-
-// const server = http.createServer((req, res) => {
-//   // req apa yg dikirim ke server dan res apa yg dikembalikan sebagai respon oleh web server
-//   //   untuk menjalankan, pamggil method listen.
-// });
-
-// server.listen(3000, () => {
-//   // yg akan menjalankan web server
-//   console.log("Server is listening on port 3000.."); // pesan yg ada di console ketika servernya/program sudah berjalan
-// });
+const port = 3000;
 
 // web server yg sederhana
 http
   .createServer((req, res) => {
-    res.write("Hello World!");
-    res.end();
-  })
-  .listen(3000, () => {
-    console.log("Server is listening on port 3000.."); // pesan yg ada di console ketika servernya/program sudah berjalan
-  });
+    res.writeHead(200, {
+      // mengembalikan tipe konten dari text biasa ke text html
+      "Content-Type": "text/html",
+    });
 
-/**
- jalankan node app di term, nanti command prompt akan muncul tetapi nyangkut karena server lagi berjalan, jika sudah seperti itu, lalu jalankan di web http://localhost:3000/
- */
+    const url = req.url;
+    // const url apapun yg dikembalikan dari request, ambil url nya
+    if (url === "/about") {
+      res.write("<h1>Ini Adalah halaman About</h1>");
+      res.end();
+    } else if (url == "/contact") {
+      res.write("<h2>Ini Adalah Halaman Contact");
+      res.end();
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}..`);
+  });
